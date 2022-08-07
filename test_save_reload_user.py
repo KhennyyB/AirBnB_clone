bbfe39592806076f96b48f1b0ab4a85b8fecb43d
BennyMbang <mbenny511@gmail.com>
2022-08-07 15:52:15 -0700

    username and password testing

--- test_save_reload_user.py
+++ test_save_reload_user.py
@@ -14,13 +14,13 @@ my_user = User()
 my_user.first_name = "Cynthia"
 my_user.last_name = "Bar"
 my_user.email = "airbnb@mail.com"
-my_user.password = "root"
+my_user.password = "alx"
 my_user.save()
 print(my_user)
 
 print("-- Create a new User 2 --")
 my_user2 = User()
-my_user2.first_name = "John"
+my_user2.first_name = "Israel"
 my_user2.email = "airbnb2@mail.com"
 my_user2.password = "root"
 my_user2.save()
